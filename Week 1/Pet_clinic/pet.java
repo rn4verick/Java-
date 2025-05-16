36adda05831d510ff78cba79e18fc227931ec925
John Maverick Rosales <maverickbrosales@gmail.com>
2025-05-16 16:14:10 +0300

    2nd Exercise

---
+++ Week 1/Pet_clinic/pet.java
@@ -0,0 +1,19 @@
+package Pet_clinic;
+
+public class pet {
+    private String species;
+    private String name;
+
+    public pet(String species, String name) {
+        this.species = species;
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
