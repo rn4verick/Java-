b60b27036ca02be8ce96ec6aa8c16101775bf8f1
John Maverick Rosales <maverickbrosales@gmail.com>
2025-05-16 15:35:54 +0300

    WEEK 1

---
+++ rectangle.java
@@ -0,0 +1,20 @@
+public class rectangle {
+    int width,height;
+
+    public rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    public void draw() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
